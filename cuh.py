e9fe1475548d1993b1f0b3de4a201363ab5718ac
SHREK <namtran10205@gmail.com>
2026-05-18 01:29:49 +0700

    cuh4

--- cuh.py
+++ cuh.py
@@ -3,4 +3,4 @@ from vision.detector import detect
 def inference(video_path, exercise, mode, user_id):
     session_id = f"sess_of_{user_id}"
     output_path, llm_response = detect(mode, video_path, exercise, user_id, session_id)
-    return output_path
+    return output_path, llm_response
